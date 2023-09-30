cffd808e816c2c13206e6c94439828ad79cafad9
alorsunique <sounique7800@gmail.com>
2023-09-30 22:04:23 +0800

    Minor changes to account list due to moving

--- Tool/account_list.py
+++ Tool/account_list.py
@@ -1,35 +1,25 @@
-import utility_pack as up
-import os
+# List the accounts in the Otter directory
+# Also shows the size of each account
 
+import os
 from pathlib import Path
 
+from Modules import utility_pack as up
 
-project_dir = Path.cwd()
-
-current_dir = project_dir
-current_dir = current_dir.parent.parent
-
-otter_dir = current_dir / "Otter"
+project_dir = Path.cwd().parent
+upper_dir = project_dir.parent.parent
+otter_dir = upper_dir / "Otter"
 
 account_dict, account_list = up.account_dictionary(otter_dir)
 
 count = 0
-
 total_size = 0
 
 for account in account_list:
-
     size = 0
-
     count += 1
-
-    # account_dir = os.path.join(account_dict[account],account)
-
     account_dir = Path(account_dict[account]) / account
-
     xz_count = 0
-
-    # for file in os.listdir(account_dir):
     for file in account_dir.iterdir():
         file_name = str(file.name)
         if ".xz" in file_name and "UTC" in file_name:
@@ -40,6 +30,6 @@ for account in account_list:
 
     print_size = up.print_size(size)
 
-    print(f"{count} | {account_dir.parent.name} | {account} | Posts Downloaded: {xz_count} | Size: {print_size}")
+    print(f"{count} | {account_dir.parent.name} | {account} | Downloaded: {xz_count} | Size: {print_size}")
 
 print(f"Total Size: {up.print_size(total_size)}")
