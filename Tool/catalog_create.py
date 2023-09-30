dda43e9377d162062b13e760c95a55da6cf724d8
alorsunique <sounique7800@gmail.com>
2023-09-30 22:04:11 +0800

    Catalog create root

--- Tool/catalog_create.py
+++ Tool/catalog_create.py
@@ -1,17 +1,9 @@
 # This script should create the CSV file that will keep track of the profiles in the Otter directory
 import os
-from pathlib import Path
 
 import pandas as pd
 
 from Modules import utility_pack as up
-
-project_dir = Path.cwd().parent
-upper_dir = project_dir.parent.parent
-otter_dir = upper_dir / "Test Otter"
-resources_dir = upper_dir / "PycharmProjects Resources" / "Piplup Resources"
-
-excel_file = resources_dir / "Test Catalog.xlsx"
 
 
 def otter_scan(otter_dir, include_status=True):
@@ -30,66 +22,53 @@ def otter_scan(otter_dir, include_status=True):
     return otter_list
 
 
-if excel_file.exists():
-    # Excel content is read here
-    import_dataframe = pd.read_excel(excel_file)
-    import_list = import_dataframe.values.tolist()
+def catalog_create(catalog_file, otter_dir):
+    if catalog_file.exists():
+        # Excel content is read here
+        import_dataframe = pd.read_excel(catalog_file)
+        import_list = import_dataframe.values.tolist()
 
-    # Reference list is created to compare entries from otter directory and entries from the excel file
-    reference_import_list = []
-    for entry in import_list:
-        reference_import_list.append(tuple(entry[:-1]))
+        # Reference list is created to compare entries from otter directory and entries from the excel file
+        reference_import_list = []
+        for entry in import_list:
+            reference_import_list.append(tuple(entry[:-1]))
 
-    # Converting to set
-    otter_set = set(otter_scan(otter_dir, False))
-    import_set = set(reference_import_list)
+        # Converting to set
+        otter_set = set(otter_scan(otter_dir, False))
+        import_set = set(reference_import_list)
 
-    difference = import_set.difference(otter_set)
-    difference_list = []
-    print("Import difference")
-    for entry in difference:
-        print(f"Difference: {entry[0]}")
-        difference_list.append(entry[0])
+        difference = import_set.difference(otter_set)
+        difference_list = []
+        # print("Import difference")
+        for entry in difference:
+            # print(f"Difference: {entry[0]}")
+            difference_list.append(entry[0])
 
-    modified_list = []
-    for entry in import_list:
-        if entry[0] not in difference_list:
-            modified_list.append(entry)
+        modified_list = []
+        for entry in import_list:
+            if entry[0] not in difference_list:
+                modified_list.append(entry)
 
-    reference_import_list = []
-    for entry in modified_list:
-        reference_import_list.append(tuple(entry[:-1]))
-
-    difference = otter_set.difference(import_set)
-
-    print("Otter difference")
-    for entry in difference:
-        print(f"Difference: {entry[0]}")
-        modified_list.append([entry[0], entry[1], 0])
-
-    unique_division = []
-    for entry in modified_list:
-        if entry[1] not in unique_division:
-            unique_division.append(entry[1])
-
-    unique_division = sorted(unique_division)
-    updated_list = []
-
-    for division in unique_division:
-        division_list = []
+        reference_import_list = []
         for entry in modified_list:
-            if entry[1] == division:
-                division_list.append(entry)
-        division_list.sort(key=lambda x: x[0])
-        updated_list.extend(division_list)
+            reference_import_list.append(tuple(entry[:-1]))
 
-    dataframe = pd.DataFrame(updated_list, columns=['Account', 'Division', 'Create Status'])
-    print(dataframe)
+        difference = otter_set.difference(import_set)
 
-    os.remove(excel_file)
-    dataframe.to_excel(excel_file, sheet_name='Catalog', index=False)
+        # print("Otter difference")
+        for entry in difference:
+            # print(f"Difference: {entry[0]}")
+            modified_list.append([entry[0], entry[1], 0])
 
-else:  # Creation of new catalog excel file
-    otter_list = otter_scan(otter_dir)
-    dataframe = pd.DataFrame(otter_list, columns=['Account', 'Division', 'Create Status'])
-    dataframe.to_excel(excel_file, sheet_name='Catalog', index=False)
+        updated_list = up.division_list_sort(modified_list)
+
+        dataframe = pd.DataFrame(updated_list, columns=['Account', 'Division', 'Create Status'])
+        # print(dataframe)
+
+        os.remove(catalog_file)
+        dataframe.to_excel(catalog_file, sheet_name='Catalog', index=False)
+
+    else:  # Creation of new catalog excel file
+        otter_list = otter_scan(otter_dir)
+        dataframe = pd.DataFrame(otter_list, columns=['Account', 'Division', 'Create Status'])
+        dataframe.to_excel(catalog_file, sheet_name='Catalog', index=False)
