4f4f06e85e6a3ba628d74ae9df9ab09611b71186
alorsunique <sounique7800@gmail.com>
2023-10-01 23:11:17 +0800

    New functionality added. Still needs cleaning

--- Tool/xz_check.py
+++ Tool/xz_check.py
@@ -2,6 +2,64 @@
 # Essentially this checks for incomplete deletion
 
 import os
+import time
+from pathlib import Path
+
+
+
+project_dir = Path.cwd().parent
+upper_dir = project_dir.parent.parent
+otter_dir = upper_dir / "Otter"
+#print(upper_dir)
+
+
+for division in otter_dir.iterdir():
+
+    for profile in division.iterdir():
+
+        xz_list = []
+
+        for file in profile.iterdir():
+            if ".xz" in file.name and "UTC" in file.name:
+                xz_list.append(file.stem.replace(".json",""))
+
+        with_image_list = []
+
+        for file in profile.iterdir():
+
+            file_name = file.name
+            if ".jpg" in file_name:
+                #print(file_name)
+                initial_entry_split = file_name.split(".")
+                entry_split = initial_entry_split[0].split("_")
+                clean_string = f"{entry_split[0]}_{entry_split[1]}_{entry_split[2]}"
+                #print(clean_string)
+                if clean_string not in with_image_list:
+                    with_image_list.append(clean_string)
+
+        xz_list_copy = xz_list.copy()
+        for image in with_image_list:
+            for xz in xz_list:
+                if image in xz:
+                    #print(f"Image: {image} | xz: {xz}")
+                    xz_list_copy.remove(xz)
+
+
+        xz_set = set(xz_list)
+        with_image_set = set(with_image_list)
+
+        print(profile.name)
+        print(xz_list_copy)
+
+        set_list = list(xz_set.difference(with_image_set))
+        set_list = sorted(set_list)
+        print(set_list)
+
+        print(list(xz_set.difference(with_image_set)).sort())
+
+
+
+time.sleep(1000)
 
 project_dir = os.getcwd()
 
@@ -31,9 +89,6 @@ for entry in os.listdir(otter_dir):
                 xz_list.append(file)
 
         # print(xz_list)
-
-        xz_list_copy = xz_list.copy()
-
         with_image_list = []
 
         for file in os.listdir(account_dir):
@@ -47,6 +102,9 @@ for entry in os.listdir(otter_dir):
 
                 if clean_string not in with_image_list:
                     with_image_list.append(clean_string)
+
+        xz_list_copy = xz_list.copy()
+
 
         for image in with_image_list:
             for xz in xz_list:
