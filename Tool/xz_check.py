e169b74576571e68f25b0e951b9f0d65090c0ea5
alorsunique <sounique7800@gmail.com>
2023-10-01 23:18:08 +0800

    xz check complete. 2023 October 01

--- Tool/xz_check.py
+++ Tool/xz_check.py
@@ -1,8 +1,6 @@
 # This compares the .xz files in the profiles to post
 # Essentially this checks for incomplete deletion
 
-import os
-import time
 from pathlib import Path
 
 project_dir = Path.cwd().parent
@@ -13,12 +11,13 @@ for division in otter_dir.iterdir():
     for profile in division.iterdir():
         xz_list = []
 
+        # Takes note of the xz files present
         for file in profile.iterdir():
             if ".xz" in file.name and "UTC" in file.name:
                 xz_list.append(file.stem.replace(".json", ""))
 
+        # Takes note of the images present
         with_image_list = []
-
         for file in profile.iterdir():
             file_name = file.name
             if ".jpg" in file_name:
@@ -28,64 +27,13 @@ for division in otter_dir.iterdir():
                 if clean_string not in with_image_list:
                     with_image_list.append(clean_string)
 
+        # Converts to list for faster manipulation
+        # The difference checks for xz files with no images corresponding to them
         xz_set = set(xz_list)
         with_image_set = set(with_image_list)
         difference = xz_set.difference(with_image_set)
 
+        # If differences are found, print the xz files that needs to be deleted
         if len(difference) > 0:
             print(profile.name)
             print(sorted(list(difference)))
-
-
-time.sleep(1000)
-
-project_dir = os.getcwd()
-
-# Change the directory to one level above
-split = project_dir.rfind("\\")
-new_dir = project_dir[0: split + 1]
-os.chdir(new_dir)
-working_dir = os.getcwd()
-
-# Takes note of the Otter directory
-
-otter_dir = os.path.join(working_dir, "Otter")
-
-for entry in os.listdir(otter_dir):
-    print(entry)
-
-    sub_folder_dir = os.path.join(otter_dir, entry)
-
-    for account in os.listdir(sub_folder_dir):
-
-        account_dir = os.path.join(sub_folder_dir, account)
-
-        xz_list = []
-
-        for file in os.listdir(account_dir):
-            if ".xz" in file and "UTC" in file:
-                xz_list.append(file)
-
-        # print(xz_list)
-        with_image_list = []
-
-        for file in os.listdir(account_dir):
-            if ".jpg" in file:
-
-                initial_entry_split = file.split(".")
-
-                entry_split = initial_entry_split[0].split("_")
-
-                clean_string = f"{entry_split[0]}_{entry_split[1]}_{entry_split[2]}"
-
-                if clean_string not in with_image_list:
-                    with_image_list.append(clean_string)
-
-        xz_list_copy = xz_list.copy()
-
-        for image in with_image_list:
-            for xz in xz_list:
-                if image in xz:
-                    xz_list_copy.remove(xz)
-
-        print(f"{account} | {xz_list_copy}")
